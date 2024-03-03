a06cb4b5f21b5adc1a69d971583c7232fc2b0bb1
Atiqur Rahman <atiqur.main@gmail.com>
2024-03-03 03:04:25 -0600

    update name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="h-[40rem] w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
       <h1 className="md:text-7xl text-3xl lg:text-9xl font-bold text-center text-white relative z-20">
-        System of Down
+        Atiqur Rahman
       </h1>
 
       <div className="w-[40rem] h-40 relative">
