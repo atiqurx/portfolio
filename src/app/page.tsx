829c9010bfebfc3be0c5a99540274c68fd5e7675
Atiqur Rahman <atiqur.main@gmail.com>
2024-03-03 03:49:55 -0600

    import floating navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,13 @@ import React from "react";
 import { SparklesCore } from "../../components/ui/sparkles";
 import Image from "next/image";
 import { SparklesPreview } from "./sparkles";
+import { FloatingNavDemo } from "./floating-navbar";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <FloatingNavDemo />
       <SparklesPreview />
     </main>
-    
-
-
   );
 }
