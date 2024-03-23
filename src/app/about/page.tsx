b51ed6d74ab9aad00e398d7f38a6658c2a57a6e0
Atiqur Rahman <atiqur.main@gmail.com>
2024-03-23 03:55:56 -0500

    add type writer effect

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,12 +1,33 @@
-import React from 'react';
+"use client";
+import { TypewriterEffectSmooth } from "../../../components/ui/typewriter-effect";
+export function AboutPage() {
+  const words = [
+    {
+      text: "Honors",
+    },
+    {
+      text: "Computer",
+    },
+    {
+      text: "Science",
+    },
+    {
+      text: "at",
+    },
+    {
+      text: "UT Arlington.",
+      className: "text-blue-500 text-indigo-500",
+    },
+  ];
 
-const AboutPage: React.FC = () => {
   return (
-    <div>
-      <h1>About Page</h1>
-      <p>This is a basic About page.</p>
+    <div className="flex flex-col items-center justify-center h-[100rem]  ">
+
+      <p className="text-neutral-600 dark:text-neutral-200 text-xs sm:text-base  ">
+
+      </p>
+      <TypewriterEffectSmooth words={words} />
     </div>
   );
-};
-
+}
 export default AboutPage;
