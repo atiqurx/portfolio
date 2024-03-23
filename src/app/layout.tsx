855f73c9483420c8f6fdad26129742b54686fd2f
Atiqur Rahman <atiqur.main@gmail.com>
2024-03-23 03:19:15 -0500

    update layout file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,14 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { FloatingNavDemo } from "./floating-navbar";
+import { FaGithub, FaKaggle, FaLinkedin } from 'react-icons/fa';
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Atiqur Rahman",
-  description: "Welcome",
+  description: "Atiqur Rahman Portfolio",
 };
 
 export default function RootLayout({
@@ -16,7 +18,46 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+
+        <div>
+          <FloatingNavDemo />
+        </div>
+        
+        {children}
+
+        <footer className="w-11/12 min-h-48 mx-auto">
+          <hr className="border-t border-stone-600"/>
+
+          <div className="flex justify-between lg:p-8 p-4 text-stone-300">
+            {/* Copyright */}
+            <div>
+              <h1>Atiqur Rahman</h1>
+              <p> &copy; 2024 Atiqur Rahman. All rights reserved.</p>
+            </div>
+
+            {/* Social Media */}
+            <div className="flex">
+              <div className="mr-4">
+                <a href="https://github.com/atiqurx" target="_blank" rel="noopener noreferrer">
+                  <FaGithub size={20} />
+                </a>
+              </div>
+              <div className="mr-4">
+                <a href="#" target="_blank" rel="noopener noreferrer">
+                  <FaKaggle size={20} />
+                </a>
+              </div>
+              <div>
+                <a href="https://www.linkedin.com/in/atiqurx" target="_blank" rel="noopener noreferrer">
+                  <FaLinkedin size={20} />
+                </a>
+              </div>
+            </div>
+          </div>
+        </footer>
+      
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,17 +3,12 @@ import React from "react";
 import { SparklesCore } from "../../components/ui/sparkles";
 import Image from "next/image";
 import { SparklesPreview } from "./sparkles";
-import { FloatingNavDemo } from "./floating-navbar";
 import { ThreeDCardDemo } from "./3d-card"
-import { FaGithub, FaKaggle, FaLinkedin } from 'react-icons/fa';
 
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between main">
-      <div>
-        <FloatingNavDemo />
-      </div>
 
       <div>
         <SparklesPreview />
@@ -36,37 +31,6 @@ export default function Home() {
       </section>
       
       
-      
-      <footer className="w-11/12 min-h-48">
-        <hr className="border-t border-stone-600"/>
-
-        <div className="flex justify-between lg:p-8 p-4 text-stone-300">
-          {/* Copyright */}
-          <div>
-            <h1>Atiqur Rahman</h1>
-            <p> &copy; 2024 Atiqur Rahman. All rights reserved.</p>
-          </div>
-
-          {/* Social Media */}
-          <div className="flex">
-            <div className="mr-4">
-              <a href="https://github.com/atiqurx" target="_blank" rel="noopener noreferrer">
-                <FaGithub size={20} />
-              </a>
-            </div>
-            <div className="mr-4">
-              <a href="#" target="_blank" rel="noopener noreferrer">
-                <FaKaggle size={20} />
-              </a>
-            </div>
-            <div>
-              <a href="https://www.linkedin.com/in/atiqurx" target="_blank" rel="noopener noreferrer">
-                <FaLinkedin size={20} />
-              </a>
-            </div>
-          </div>
-        </div>
-      </footer>
     </main>
   );
 }
