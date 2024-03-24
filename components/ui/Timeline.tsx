2e8dcd1f5f4220c399c156bf4d19a51bf880135b
Atiqur Rahman <atiqur.main@gmail.com>
2024-03-23 21:14:03 -0500

    add icons to timeline

--- components/ui/Timeline.tsx
+++ components/ui/Timeline.tsx
@@ -5,20 +5,21 @@ import TimelineSeparator from "@mui/lab/TimelineSeparator";
 import TimelineConnector from "@mui/lab/TimelineConnector";
 import TimelineContent from "@mui/lab/TimelineContent";
 import TimelineDot from "@mui/lab/TimelineDot";
+import EventIcon from "@mui/icons-material/Event";
 
 export default function BasicTimeline() {
   return (
     <Timeline>
       <TimelineItem>
         <TimelineSeparator>
-          <TimelineDot variant="outlined" color="primary"  />
+          <TimelineDot variant="outlined" color="primary" />
           <TimelineConnector />
         </TimelineSeparator>
         <TimelineContent>
           <div>
             <h1>Developement Lead</h1>
             <p>UTA Datathon</p>
-            <p></p>
+            <p className="mt-2"><EventIcon fontSize="small" />  Feb 2024 - Present</p>
           </div>
         </TimelineContent>
       </TimelineItem>
@@ -28,10 +29,10 @@ export default function BasicTimeline() {
           <TimelineConnector />
         </TimelineSeparator>
         <TimelineContent>
-        <div>
+          <div>
             <h1>Undergraduate Research Assistant</h1>
             <p>University of Texas at Arlington </p>
-            <p></p>
+            <p className="mt-2"> <EventIcon fontSize="small" />  Feb 2024 - Present</p>
           </div>
         </TimelineContent>
       </TimelineItem>
@@ -41,10 +42,10 @@ export default function BasicTimeline() {
           <TimelineConnector />
         </TimelineSeparator>
         <TimelineContent>
-        <div>
+          <div>
             <h1>Supplemental Instruction Leader - CSE 1310</h1>
             <p>UTA Academic Success Center</p>
-            <p></p>
+            <p className="mt-2"> <EventIcon fontSize="small" />  Aug 2023 - Present</p>
           </div>
         </TimelineContent>
       </TimelineItem>
@@ -53,16 +54,13 @@ export default function BasicTimeline() {
           <TimelineDot />
         </TimelineSeparator>
         <TimelineContent>
-          
           <div>
             <h1>Orientation Leader</h1>
             <p>UTA New Maverick Orientation</p>
-            <p></p>
+            <p className="mt-2"> <EventIcon fontSize="small" />  Jan 2023 - Aug 2023</p>
           </div>
         </TimelineContent>
       </TimelineItem>
-      
-      
     </Timeline>
   );
 }
