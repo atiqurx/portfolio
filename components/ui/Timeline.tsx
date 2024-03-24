89f439d16a9230fcc9f9f2950df4bf8ccfea6ab3
Atiqur Rahman <atiqur.main@gmail.com>
2024-03-23 20:53:18 -0500

    update timeline

--- components/ui/Timeline.tsx
+++ components/ui/Timeline.tsx
@@ -1,34 +1,68 @@
-import * as React from 'react';
-import Timeline from '@mui/lab/Timeline';
-import TimelineItem from '@mui/lab/TimelineItem';
-import TimelineSeparator from '@mui/lab/TimelineSeparator';
-import TimelineConnector from '@mui/lab/TimelineConnector';
-import TimelineContent from '@mui/lab/TimelineContent';
-import TimelineDot from '@mui/lab/TimelineDot';
+import * as React from "react";
+import Timeline from "@mui/lab/Timeline";
+import TimelineItem from "@mui/lab/TimelineItem";
+import TimelineSeparator from "@mui/lab/TimelineSeparator";
+import TimelineConnector from "@mui/lab/TimelineConnector";
+import TimelineContent from "@mui/lab/TimelineContent";
+import TimelineDot from "@mui/lab/TimelineDot";
 
 export default function BasicTimeline() {
   return (
     <Timeline>
       <TimelineItem>
         <TimelineSeparator>
-          <TimelineDot />
+          <TimelineDot variant="outlined" color="primary"  />
           <TimelineConnector />
         </TimelineSeparator>
-        <TimelineContent>Eat</TimelineContent>
+        <TimelineContent>
+          <div>
+            <h1>Developement Lead</h1>
+            <p>UTA Datathon</p>
+            <p></p>
+          </div>
+        </TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineDot variant="outlined" color="primary" />
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>
+        <div>
+            <h1>Undergraduate Research Assistant</h1>
+            <p>University of Texas at Arlington </p>
+            <p></p>
+          </div>
+        </TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineDot variant="outlined" color="primary" />
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>
+        <div>
+            <h1>Supplemental Instruction Leader - CSE 1310</h1>
+            <p>UTA Academic Success Center</p>
+            <p></p>
+          </div>
+        </TimelineContent>
       </TimelineItem>
       <TimelineItem>
         <TimelineSeparator>
           <TimelineDot />
-          <TimelineConnector />
         </TimelineSeparator>
-        <TimelineContent>Code</TimelineContent>
+        <TimelineContent>
+          
+          <div>
+            <h1>Orientation Leader</h1>
+            <p>UTA New Maverick Orientation</p>
+            <p></p>
+          </div>
+        </TimelineContent>
       </TimelineItem>
-      <TimelineItem>
-        <TimelineSeparator>
-          <TimelineDot />
-        </TimelineSeparator>
-        <TimelineContent>Sleep</TimelineContent>
-      </TimelineItem>
+      
+      
     </Timeline>
   );
 }
