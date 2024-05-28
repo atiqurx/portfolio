92ed47b1c84e9c09f09d9d714ec4ebf58fe1b37b
Atiqur Rahman <atiqur.main@gmail.com>
2024-05-28 13:38:43 -0500

    add resume and link it

--- components/ui/floating-navbar.tsx
+++ components/ui/floating-navbar.tsx
@@ -79,10 +79,15 @@ export const FloatingNav = ({
             </a>
           )
         ))}
-        <button className="border text-sm font-medium relative border-neutral-200 dark:border-white/[0.2] text-black dark:text-white px-4 py-2 rounded-full">
+        <a
+          href="/Atiqur_Rahman_Resume.pdf"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="border text-sm font-medium relative border-neutral-200 dark:border-white/[0.2] text-black dark:text-white px-4 py-2 rounded-full"
+        >
           <span>Resume</span>
-          <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent  h-px" />
-        </button>
+          <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent h-px" />
+        </a>
       </motion.div>
     </AnimatePresence>
   );
